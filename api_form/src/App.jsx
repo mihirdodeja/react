9f283d199286fed9e738a0083ecb5d3b3c5cd91a
mihirdodeja <mihirdodeja2004@gmail.com>
2026-04-14 11:16:45 +0530

    Form Data using API

---
+++ api_form/src/App.jsx
@@ -0,0 +1,11 @@
+import { Form } from "react-hook-form"
+import ProductForm from "./ProductForm"
+
+ const App = () => {
+  return (
+    <>
+        <ProductForm/>
+    </>
+  )
+}
+export default App
